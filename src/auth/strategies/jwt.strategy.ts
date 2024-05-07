d1851cc99defd1a88aa0078974682d3094c70c7f
Viral Rupani <viralrupani12017@gmail.com>
2024-05-07 21:46:51 +0530

    fix: add jwt auth user validation

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,12 +1,21 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { IPayload } from '../auth.interfaces';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { User } from 'src/users/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityNotFoundError, Repository } from 'typeorm';
+import { Message } from 'src/common/constants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private readonly config: ConfigService) {
+    constructor(
+        private readonly config: ConfigService,
+
+        @InjectRepository(User)
+        private readonly userRepo: Repository<User>,
+    ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
@@ -14,7 +23,18 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         });
     }
 
-    validate(payload: IPayload) {
-        return payload;
+    async validate(payload: IPayload) {
+        try {
+            await this.userRepo.findOneByOrFail({
+                email: payload?.email,
+            });
+            return payload;
+        } catch (error) {
+            if (error instanceof EntityNotFoundError) {
+                throw new UnauthorizedException(Message.USER_NOT_FOUND);
+            } else {
+                throw error;
+            }
+        }
     }
 }
